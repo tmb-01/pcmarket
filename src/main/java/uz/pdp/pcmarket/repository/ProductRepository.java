a87620b7d419ea8231c016e9c8c740b41782012d
Maraim Tuxtasunov <maraimtuxtasunov@gmail.com>
2021-06-03 11:04:45 +0500

    added filter

--- src/main/java/uz/pdp/pcmarket/repository/ProductRepository.java
+++ src/main/java/uz/pdp/pcmarket/repository/ProductRepository.java
@@ -1,11 +1,20 @@
 package uz.pdp.pcmarket.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import uz.pdp.pcmarket.entity.Product;
 import uz.pdp.pcmarket.projection.CartProjection;
 import uz.pdp.pcmarket.projection.ProductProjection;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "product", collectionResourceRel = "list", excerptProjection = ProductProjection.class)
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @RestResource(path = "price-between")
+    Page<Product> findAllByPriceBetween(int price, int price2, Pageable pageable);
+
 }
--- src/main/java/uz/pdp/pcmarket/repository/PropertyRepository.java
+++ src/main/java/uz/pdp/pcmarket/repository/PropertyRepository.java
@@ -1,7 +1,16 @@
 package uz.pdp.pcmarket.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import uz.pdp.pcmarket.entity.Property;
+import uz.pdp.pcmarket.projection.PropertyProjection;
 
+@RepositoryRestResource(path = "property", collectionResourceRel = "list", excerptProjection = PropertyProjection.class)
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    @RestResource(path = "byProperty")
+    Page<Property> findAllByPropertyNameAndPropertyValue(String propertyName, String propertyValue, Pageable pageable);
+
 }
